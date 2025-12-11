ef7fbc15b244a0a7471da34a90c6e25affe247d3
Csaba Paronai <paronai.csaba@gmail.com>
2025-12-11 21:37:48 +0000

    2025 Day 8

--- 2025/08/main.py
+++ 2025/08/main.py
@@ -28,67 +28,99 @@ class Connection:
         return self.distance < other.distance
 
 
-def get_distance_matrix(coordinates):
+def load_coordinates(filename):
+    coordinates = []
+    with open(filename, "r") as file:
+        for line in file:
+            x, y, z = line.strip().split(",")
+            coordinates.append(Coordinate(int(x), int(y), int(z)))
+    return coordinates
+
+
+def calculate_distance(coord1, coord2):
+    return math.sqrt((coord1.x - coord2.x) ** 2 + (coord1.y - coord2.y) ** 2 + (coord1.z - coord2.z) ** 2)
+
+
+def build_distance_heap(coordinates):
     heap = []
     heapify(heap)
-    for (index_from, from_coordinate) in enumerate(coordinates):
-        for (index_to, to_coordinate) in enumerate(coordinates[index_from + 1:]):
-            distance = math.sqrt(
-                (from_coordinate.x - to_coordinate.x) ** 2 + (from_coordinate.y - to_coordinate.y) ** 2 + (
-                        from_coordinate.z - to_coordinate.z) ** 2)
-            heappush(heap, Connection(from_coordinate, to_coordinate, distance))
+
+    for index_from, from_coord in enumerate(coordinates):
+        for to_coord in coordinates[index_from + 1:]:
+            heappush(heap, Connection(from_coord, to_coord, calculate_distance(from_coord, to_coord)))
+
     return heap
 
 
-def process_input():
-    with open("input.txt", "r") as file:
-        coordinates = []
-        for line in file:
-            xyz = line.strip().split(",")
-            coordinates.append(Coordinate(int(xyz[0]), int(xyz[1]), int(xyz[2])))
-        return coordinates
+def merge_or_add_connection(circuits, connection, nodes_connected, total_nodes):
+    from_circuit = None
+    to_circuit = None
 
+    for circuit in circuits:
+        if connection.from_coordinate in circuit and connection.to_coordinate in circuit:
+            return 0, None
+        if connection.from_coordinate in circuit:
+            from_circuit = circuit
+        elif connection.to_coordinate in circuit:
+            to_circuit = circuit
 
-def add_to_circuits(circuits, connection):
-    from_circuit_match = None
-    to_circuit_match = None
-    for existing_circuit in circuits:
-        if connection.from_coordinate in existing_circuit and connection.to_coordinate in existing_circuit:
-            return
-        if connection.from_coordinate in existing_circuit:
-            from_circuit_match = existing_circuit
-        elif connection.to_coordinate in existing_circuit:
-            to_circuit_match = existing_circuit
-
-    if from_circuit_match is not None and to_circuit_match is not None:
-        circuits.remove(to_circuit_match)
-        for to_circuit in to_circuit_match:
-            from_circuit_match.add(to_circuit)
-    elif from_circuit_match is not None:
-        from_circuit_match.add(connection.to_coordinate)
-    elif to_circuit_match is not None:
-        to_circuit_match.add(connection.from_coordinate)
+    newly_connected_nodes = 0
+    if from_circuit is not None and to_circuit is not None:
+        circuits.remove(to_circuit)
+        from_circuit.update(to_circuit)
+    elif from_circuit is not None:
+        from_circuit.add(connection.to_coordinate)
+        newly_connected_nodes = 1
+    elif to_circuit is not None:
+        to_circuit.add(connection.from_coordinate)
+        newly_connected_nodes = 1
     else:
         circuits.append({connection.from_coordinate, connection.to_coordinate})
+        newly_connected_nodes = 2
+
+    result2 = None
+    if len(circuits) == 1 and nodes_connected + newly_connected_nodes == total_nodes:
+        result2 = connection.from_coordinate.x * connection.to_coordinate.x
+
+    return newly_connected_nodes, result2
 
 
-(distance_matrix, heap) = get_distance_matrix(process_input())
-counter = 0
-circuits = []
-while counter < min(1000, len(heap)):
-    smallest_distance_connection = heappop(heap)
-    add_to_circuits(circuits, smallest_distance_connection)
-    counter += 1
+def calculate_top3_product(circuits):
+    heap = []
+    heapify(heap)
 
-heap2 = []
-heapify(heap2)
-for circuit in circuits:
-    if len(heap2) < 3:
-        heappush(heap2, len(circuit))
-    elif len(circuit) > heap2[0]:
-        heapreplace(heap2, len(circuit))
+    for circuit in circuits:
+        if len(heap) < 3:
+            heappush(heap, len(circuit))
+        elif len(circuit) > heap[0]:
+            heapreplace(heap, len(circuit))
 
-result = 1
-while len(heap2) > 0:
-    result *= heappop(heap2)
-print(result)
+    product = 1
+    while len(heap) > 0:
+        product *= heappop(heap)
+    return product
+
+
+MAX_CONNECTIONS = 1000
+
+
+def main():
+    nodes = load_coordinates("input.txt")
+    distance_heap = build_distance_heap(nodes)
+    circuits = []
+    nodes_connected = 0
+    counter = 0
+    while counter < len(distance_heap):
+        connection = heappop(distance_heap)
+        newly_connected, result2 = merge_or_add_connection(circuits, connection, nodes_connected, len(nodes))
+        nodes_connected += newly_connected
+        if counter == MAX_CONNECTIONS:
+            print("Result 1", calculate_top3_product(circuits))
+        if result2 is not None:
+            print("Result 2", result2)
+            break
+        counter += 1
+
+
+if __name__ == "__main__":
+    main()
